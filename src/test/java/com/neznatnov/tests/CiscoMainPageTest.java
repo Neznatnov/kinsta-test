f0d543c945f530845975257c50870388269a3235
Veronika Iatckaia <neznatnov@gmail.com>
2023-06-26 18:22:06 +0200

    add task with tags

--- src/test/java/com/neznatnov/tests/CiscoMainPageTest.java
+++ src/test/java/com/neznatnov/tests/CiscoMainPageTest.java
@@ -3,6 +3,7 @@ package com.neznatnov.tests;
 import com.codeborne.selenide.Selenide;
 import com.neznatnov.pages.CiscoMainPage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -11,6 +12,7 @@ public class CiscoMainPageTest extends TestBase {
     CiscoMainPage ciscoMainPage = new CiscoMainPage();
 
     @Test
+    @Tag("test")
     @DisplayName("Check logo exists on the main page")
     void logoExist() {
         step("Open website kinsta", () -> {
@@ -22,6 +24,7 @@ public class CiscoMainPageTest extends TestBase {
     }
 
     @Test
+    @Tag("test")
     @DisplayName("Check search text placeholder on the main page")
     void searchExist() {
         step("Open website kinsta", () -> {
@@ -33,12 +36,14 @@ public class CiscoMainPageTest extends TestBase {
     }
 
     @Test
+    @Tag("test")
     @DisplayName("Check video, INPROCESS")
     void videoPlaying() {
         ciscoMainPage.openPage().videoPlaying();
     }
 
     @Test
+    @Tag("test")
     @DisplayName("Check language change on main page, german")
     void changeLanguage() {
         step("Open website kinsta", () -> {
@@ -59,6 +64,7 @@ public class CiscoMainPageTest extends TestBase {
     }
 
     @Test
+    @Tag("test")
     @DisplayName("Check the correct text in Contact bar on main page")
     void contactBar() {
         step("Open website kinsta", () -> {
